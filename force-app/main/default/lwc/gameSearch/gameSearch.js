c1ae6761ca6ead348ff981b458aeded84f3abe38
Matthew Lewandowski <mlewandowski96@gmail.com>
2022-09-25 17:46:22 -0400

    made it look prettier and added color coding to the results

--- force-app/main/default/lwc/gameSearch/gameSearch.js
+++ force-app/main/default/lwc/gameSearch/gameSearch.js
@@ -2,11 +2,12 @@ import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class GameSearch extends LightningElement {
-    @track games = [];
-    @track filteredGames = null;
+    @track games;
+    @track filteredGames;
 
-    //sortingDropDown Variable
-    value = 'aToZ';
+    //keeping track of filtering and sorting conditions
+    sortingCondition = 'aToZ';
+    filteringCondition = 'none';
 
     //Modal Variables
     displayModal = false;
@@ -23,10 +24,14 @@ export default class GameSearch extends LightningElement {
                 //keep a full version of the list of games for filtering/sorting reasons
                 this.games = data;
                 this.filteredGames = this.games;
-
+                
+                for(let game of this.filteredGames) {
+                    this.evaluateRating(game);
+                    console.log(game.dealRating);
+                }
                 //sort and filter the list according to currently selected values
-                this.filterPrice(this.template.querySelector('lightning-combobox[data-id=filterDropdown]').value);
-                this.sort(this.template.querySelector('lightning-combobox[data-id=sortingDropdown]').value);
+                this.filterPrice(this.filteringCondition);
+                this.sort(this.sortingCondition);
             })
             .catch((error) => {
                 const event = new ShowToastEvent({
@@ -43,13 +48,12 @@ export default class GameSearch extends LightningElement {
     displayMoreInfo(event) {
         this.displayModal = true;
 
-        //filter the games array to get the object that they clicked on
-        let currentObject = this.games.filter(game => (game.title == event.target.textContent && 
-                                                       game.storeID == event.target.title));
+        //find the game object clicked on using title field on the html a tag
+        let currentObject = this.games.find(game => game.dealID == event.target.title);
         
         //display the info from the object
-        this.gameName = currentObject[0].title;
-        this.gameImage = currentObject[0].thumb;
+        this.gameName = currentObject.title;
+        this.gameImage = currentObject.thumb;
     }
 
     displayLessInfo(event) {
@@ -57,47 +61,46 @@ export default class GameSearch extends LightningElement {
     }
 
     sortGames(event) {
-        this.sort(event.target.value);
+        this.sortingCondition = event.target.value;
+        this.sort(this.sortingCondition);
     }
 
     filterGamesPrice(event) {
-        this.filterPrice(event.target.value);
+        this.filteringCondition = event.target.value;
+        this.filterPrice(this.filteringCondition);
     }
 s
     filterPrice(filterValue) {
             //make variables to track the filter price ranges
             let lowerBound;
             let upperBound;
-
-            //grab the current sorting condition in order to re-sort after the filter
-            let sortingCondition = this.template.querySelector('lightning-combobox[data-id=sortingDropdown]').value;
     
             switch(filterValue) {
                 case 'none':
                     this.filteredGames = this.games;
-                    this.sort(sortingCondition);
+                    this.sort(this.sortingCondition);
                     break;
                 case 'fifteenMinus':
                     upperBound = 15;
                     this.filteredGames = this.games.filter(game => game.salePrice <= upperBound);
-                    this.sort(sortingCondition);
+                    this.sort(this.sortingCondition);
                     break;
                 case 'fifteentoThirty':
                     lowerBound = 15;
                     upperBound = 30;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound && game.salePrice < upperBound);
-                    this.sort(sortingCondition);
+                    this.sort(this.sortingCondition);
                     break;
                 case 'thirtyToFortyFive':
                     lowerBound = 30;
                     upperBound = 45;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound && game.salePrice < upperBound);
-                    this.sort(sortingCondition);
+                    this.sort(this.sortingCondition);
                     break;
                 case 'fortyFivePlus':
                     lowerBound = 45;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound);
-                    this.sort(sortingCondition);
+                    this.sort(this.sortingCondition);
                     break;
             }
     }
@@ -118,6 +121,22 @@ s
                     this.filteredGames.sort((a,b) => (parseFloat(a.salePrice) < parseFloat(b.salePrice)) ? 1 : ((parseFloat(b.salePrice) < parseFloat(a.salePrice)) ? -1 : 0));
                     break;
             }
+        }
+    }
+
+    evaluateRating(game) {
+        if (game.dealRating >= 7) {
+            game.highRating = true;
+            game.medRating = false;
+            game.lowRating = false;
+        } else if (game.dealRating < 7 && game.dealRating >= 4) {
+            game.highRating = false;
+            game.medRating = true;
+            game.lowRating = false;
+        } else {
+            game.highRating = false;
+            game.medRating = false;
+            game.lowRating = true;
         }
     }
 
