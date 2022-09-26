12e0f4bc2d265b0128537311dbd7cc1124ee69b1
Matthew Lewandowski <mlewandowski96@gmail.com>
2022-09-25 23:22:32 -0400

    added more data to the modal

--- force-app/main/default/lwc/gameSearch/gameSearch.js
+++ force-app/main/default/lwc/gameSearch/gameSearch.js
@@ -2,6 +2,7 @@ import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class GameSearch extends LightningElement {
+    @track stores;
     @track games;
     @track filteredGames;
 
@@ -13,6 +14,14 @@ export default class GameSearch extends LightningElement {
     displayModal = false;
     gameName;
     gameImage;
+    gameRating;
+    normalPrice;
+    salePrice;
+    steamRating;
+    steamLink;
+    metacriticRating;
+    metacriticLink;
+    storeName;
 
     searchGames(event) {
         let searchInput = this.template.querySelector('lightning-input').value;
@@ -27,11 +36,11 @@ export default class GameSearch extends LightningElement {
                 
                 for(let game of this.filteredGames) {
                     this.evaluateRating(game);
-                    console.log(game.dealRating);
                 }
+                console.log(this.filteredGames[0]);
                 //sort and filter the list according to currently selected values
-                this.filterPrice(this.filteringCondition);
-                this.sort(this.sortingCondition);
+                this.filterPrice();
+                this.sort();
             })
             .catch((error) => {
                 const event = new ShowToastEvent({
@@ -50,10 +59,50 @@ export default class GameSearch extends LightningElement {
 
         //find the game object clicked on using title field on the html a tag
         let currentObject = this.games.find(game => game.dealID == event.target.title);
+        console.log(currentObject);
+
+        fetch('https://www.cheapshark.com/api/1.0/stores')
+        .then(res => res.json())
+        .then(data => {
+            this.stores = data;
+            if(this.stores) {
+                //unfortunately the API does not provide a link to store site so we just get name
+                this.storeName = this.stores.find(store => store.storeID == currentObject.storeID).storeName;
+            }
+        })
+        .catch(error => {
+            const event = new ShowToastEvent({
+                title: 'An error occured trying to get store info',
+                message:
+                    'some data may not be available',
+                variant: 'error'
+            });
+            this.dispatchEvent(event);
+            this.stores = null;
+        })
         
         //display the info from the object
         this.gameName = currentObject.title;
         this.gameImage = currentObject.thumb;
+        this.gameRating = currentObject.dealRating;
+        this.normalPrice = currentObject.normalPrice;
+        this.salePrice = currentObject.salePrice;
+        if (currentObject.steamRatingPercent == '0') {
+            this.steamRating = null;
+        } else {
+            this.steamRating = currentObject.steamRatingPercent;
+        }
+        if(currentObject.metacriticScore == '0') {
+            this.metacriticRating = null;
+        } else {
+            this.metacriticRating = currentObject.metacriticScore;
+        }
+        if (currentObject.steamAppID) {
+            this.steamLink = 'https://store.steampowered.com/app/' + currentObject.steamAppID;
+        }
+        if(currentObject.metacriticLink) {
+            this.metacriticLink = 'https://www.metacritic.com/' + currentObject.metacriticLink;
+        }
     }
 
     displayLessInfo(event) {
@@ -70,44 +119,44 @@ export default class GameSearch extends LightningElement {
         this.filterPrice(this.filteringCondition);
     }
 s
-    filterPrice(filterValue) {
+    filterPrice() {
             //make variables to track the filter price ranges
             let lowerBound;
             let upperBound;
     
-            switch(filterValue) {
+            switch(this.filteringCondition) {
                 case 'none':
                     this.filteredGames = this.games;
-                    this.sort(this.sortingCondition);
+                    this.sort();
                     break;
                 case 'fifteenMinus':
                     upperBound = 15;
                     this.filteredGames = this.games.filter(game => game.salePrice <= upperBound);
-                    this.sort(this.sortingCondition);
+                    this.sort();
                     break;
                 case 'fifteentoThirty':
                     lowerBound = 15;
                     upperBound = 30;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound && game.salePrice < upperBound);
-                    this.sort(this.sortingCondition);
+                    this.sort();
                     break;
                 case 'thirtyToFortyFive':
                     lowerBound = 30;
                     upperBound = 45;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound && game.salePrice < upperBound);
-                    this.sort(this.sortingCondition);
+                    this.sort();
                     break;
                 case 'fortyFivePlus':
                     lowerBound = 45;
                     this.filteredGames = this.games.filter(game => game.salePrice >= lowerBound);
-                    this.sort(this.sortingCondition);
+                    this.sort();
                     break;
             }
     }
 
-    sort(sortCondition) {
+    sort() {
         if(this.filteredGames) {
-            switch(sortCondition) {
+            switch(this.sortingCondition) {
                 case 'aToZ':
                     this.filteredGames.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
                     break;
